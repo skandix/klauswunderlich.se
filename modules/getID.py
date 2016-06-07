438921ff2d23c5279790bb173872fa0f2558cc87
Bendik <bendik.dyrli@gmail.com>
2016-06-08 00:09:22 +0200

    Changes to be committed: Changes not staged for commit: Untracked files:

--- modules/getID.py
+++ modules/getID.py
@@ -1,10 +1,29 @@
 # -*- coding: utf-8 -*-
-import requests
+import re
+import urllib2
 
-def get_latest_json(endpoint):                                                           
-    return requests.get(endpoint, stream=True, headers=headers).json()
+#all the added here
+onlyid = []
+# then filter out so each id only occurs once.
+onetime = []
+def getHTML(url):
+	return urllib2.urlopen(url).read()
 
-def get_id(s):
-	yt_id = get_latest_json("https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=PL0LVG0VoEMm1EIsMin90Am-ddlpMaUZ-J&key=AIzaSyAbNu_4U5sh1jXW5VNzAjU3IJSE8encYWY")
-	for i in yt_id:
-		print i
+def getID(url, s):
+	temp = ""
+	rawHTML = getHTML(url)
+	black_magic = re.compile(ur'\/watch.[v=]+[^+]{11}')
+	loot = re.findall(black_magic, rawHTML)
+	for i in loot:
+		clean = i.replace('/watch?v=', '')
+		onlyid.append(clean)
+		s += str([x for x in onlyid if onlyid.count(x)==1]).replace("[]", '').replace('/watch_videoshelf":', '').replace("['']", '')
+		temp = s
+	temp.append(onetime)
+	print onetime
+	# fix (AttributeError: 'str' object has no attribute 'append')
+
+
+
+#getID()
+getID("https://www.youtube.com/playlist?list=PLq_0uf5RiXNoX6-DfIJ8R1su2eQp0aNg8", "")
--- template/youtube.py
+++ template/youtube.py
@@ -2,9 +2,9 @@ from string import Template
 HTML_TEMPLATE = Template("""
       <h2>
         YouTube video link: 
-        <a href="https://www.youtube.com/watch?v=${yt_id }">
-          ${yt_id }
+        <a href="https://www.youtube.com/watch?v=${yt_id}">
+          ${yt_id}
         </a>
       </h2>
     
-      <iframe src="https://www.youtube.com/embed/${yt_id }" width="853" height="480" frameborder="0" allowfullscreen></iframe>""")
+      <iframe src="https://www.youtube.com/embed/${yt_id}" width="853" height="480" frameborder="0" allowfullscreen></iframe>""")
