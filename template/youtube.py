4ad84e77be66fc74bb1d86f34d1c7505c18a7a62
bendikegenesdyrli <skandix@hypothalamus>
2016-06-09 12:18:53 +0200

    removal

--- template/youtube.py
+++
@@ -1,9 +0,0 @@
-from string import Template
-HTML_TEMPLATE = Template("""<center><p>
-        YouTube video link: 
-        <a href="https://www.youtube.com/watch?v=${yt_id}?autoplay=1">
-        </a>
-      </p>
-    
-      <iframe src="https://www.youtube.com/embed/${yt_id}?autoplay=1" width="853" height="480" frameborder="0" allowfullscreen></iframe></center>
-    """)
