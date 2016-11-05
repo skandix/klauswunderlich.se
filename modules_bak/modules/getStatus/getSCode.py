323f0214235d1b9e451635e6c67dd33d4a4040f0
skandix <skandix@skandixGit>
2016-11-05 23:53:08 +0100

    fixed

--- modules_bak/modules/getStatus/getSCode.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def is_url_ok(url):
-	return 200 == requests.head(url).status_code
-
-#print ("https://www.youtube.com/watch?v=hopYXDBQIso")
-#print requests.head("https://www.youtube.com/watch?v=hopYXDBQIso").status_code
